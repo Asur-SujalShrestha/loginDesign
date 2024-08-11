85bc401180dd2c310b6cf2722955b0ff28535c85
unknown <shresthasujal519@gmail.com>
2024-08-11 12:18:23 +0545

    second

--- src/login-page/login-page.component.ts
+++ src/login-page/login-page.component.ts
@@ -12,6 +12,11 @@ import {ErrorStateMatcher} from '@angular/material/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faLock} from '@fortawesome/free-solid-svg-icons';
+
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -23,7 +28,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [MatSlideToggleModule,FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatCheckboxModule],
+  imports: [MatSlideToggleModule,FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatCheckboxModule, FontAwesomeModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
@@ -31,4 +36,9 @@ export class LoginPageComponent {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(8),Validators.maxLength(16)]);
   matcher = new MyErrorStateMatcher();
+
+  faGoogle = faGoogle
+  faFacebook = faFacebook
+  faEnvelope = faEnvelope
+  faLock = faLock
 }
